113d8c9ffc2b78167559300d95f7092078d373ed
Steve Springett <steve@springett.us>
2023-11-27 23:03:28 -0600

    added link to ecma

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto divide-y divide-slate-200 px-4 sm:px-6 md:px-8 dark:divide-slate-700">
         <div className="flex flex-col md:flex-row justify-between text-center">
           <div className="pt-10 mx-auto md:mx-0">
-            <img src="/images/ecma.svg" alt="Ecma Logo" width="200" />
+            <a href="https://ecma-international.org/"><img src="/images/ecma.svg" alt="Ecma Logo" width="200" /></a>
           </div>
           <div className="pt-10 mb-auto mt-auto md:mx-0">
             &copy; Ecma International
