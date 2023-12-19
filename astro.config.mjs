0edf0ab3369c1218829b66b9c24a137a50877f60
Steve Springett <steve@springett.us>
2023-12-18 19:36:27 -0600

    Added sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [react(), tailwind()],
+  site: 'https://tc54.org',
+  integrations: [react(), tailwind(), sitemap()],
   markdown: {
     syntaxHighlight: 'prism'
   }
